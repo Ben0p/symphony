e89b6db6c3a4c10549593a30bbad3605649ba6bd
Ben0p <31563479+Ben0p@users.noreply.github.com>
2026-09-13 14:57:27 +0800

    test: keep expiry assertion within style limit

--- elixir/test/symphony_elixir/work_package_claim_recovery_test.exs
+++ elixir/test/symphony_elixir/work_package_claim_recovery_test.exs
@@ -341,7 +341,8 @@ defmodule SymphonyElixir.WorkPackageClaimRecoveryTest do
         (Map.get(event, :type) || Map.get(event, "type")) in [:expired, "expired"]
       end)
 
-    assert Enum.sort(Enum.map(expiry_events, &(Map.get(&1, :delegation_id) || Map.get(&1, "delegation_id")))) == Enum.sort([parent, context.delegation])
+    expired_ids = Enum.map(expiry_events, &(Map.get(&1, :delegation_id) || Map.get(&1, "delegation_id")))
+    assert Enum.sort(expired_ids) == Enum.sort([parent, context.delegation])
     unrelated_ids = Map.keys(restarted.responsibility_graph.delegations) -- [parent, context.delegation]
     assert Map.take(recovered.responsibility_graph.delegations, unrelated_ids) == Map.take(restarted.responsibility_graph.delegations, unrelated_ids)
     assert {:ok, same_graph, %{expired: []}} = ResponsibilityGraph.reconcile(recovered.responsibility_graph, System.system_time(:millisecond))
